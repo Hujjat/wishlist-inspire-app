b1a77d416ad49eff3f4fde54ec609658723c72b6
Hujjat Nazari <Hujjat@users.noreply.github.com>
2024-01-17 20:57:16 +1100

    app settings updated

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -1,22 +1,15 @@
 import {
   Box,
   Card,
-  Layout,
-  Link,
-  List,
   Page,
   Text,
   BlockStack,
   InlineGrid,
-  Divider,
   TextField,
-  useBreakpoints,
-
 } from "@shopify/polaris";
 
 export default function SettingsPage() {
 
-  const { smUp } = useBreakpoints();
   return (
     <Page>
       <ui-title-bar title="Settings" />
@@ -29,61 +22,23 @@ export default function SettingsPage() {
           >
             <BlockStack gap="400">
               <Text as="h3" variant="headingMd">
-                InterJambs
+                Settings
               </Text>
               <Text as="p" variant="bodyMd">
-                Interjambs are the rounded protruding bits of your puzzlie piece
+                Update app settings and preferences.
               </Text>
             </BlockStack>
           </Box>
           <Card roundedAbove="sm">
             <BlockStack gap="400">
-              <TextField label="Interjamb style" />
-              <TextField label="Interjamb ratio" />
+              <TextField label="App name" />
+              <TextField label="Description" />
             </BlockStack>
           </Card>
         </InlineGrid>
-        {smUp ? <Divider /> : null}
-        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
-          <Box
-            as="section"
-            paddingInlineStart={{ xs: 400, sm: 0 }}
-            paddingInlineEnd={{ xs: 400, sm: 0 }}
-          >
-            <BlockStack gap="400">
-              <Text as="h3" variant="headingMd">
-                Dimensions
-              </Text>
-              <Text as="p" variant="bodyMd">
-                Interjambs are the rounded protruding bits of your puzzlie piece
-              </Text>
-            </BlockStack>
-          </Box>
-          <Card roundedAbove="sm">
-            <BlockStack gap="400">
-              <TextField label="Horizontal" />
-              <TextField label="Interjamb ratio" />
-            </BlockStack>
-          </Card>
-        </InlineGrid>
+
+
       </BlockStack>
     </Page>
-  );
-}
-
-function Code({ children }) {
-  return (
-    <Box
-      as="span"
-      padding="025"
-      paddingInlineStart="100"
-      paddingInlineEnd="100"
-      background="bg-surface-active"
-      borderWidth="025"
-      borderColor="border"
-      borderRadius="100"
-    >
-      <code>{children}</code>
-    </Box>
   );
 }
